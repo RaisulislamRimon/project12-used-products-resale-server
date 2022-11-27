cfbd1ceb69b99174a905fcf0b975048dec5a82bf
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-27 13:57:59 +0600

    show books from mongodb by category name

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hbpron4.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const booksCategories = client
+      .db(`used-products-resale`)
+      .collection(`books-categories`);
+    const novelBooks = client
+      .db(`used-products-resale`)
+      .collection(`books`);
+
+    app.get("/books-categories", async (req, res) => {
+      const query = {};
+      const cursor = await booksCategories.find(query).toArray();
+      res.send(cursor);
+    });
+
+    app.get("/books/:category_name", async (req, res) => {
+      const category_name = req.params.category_name;
+      const query = { category_name: category_name };
+      const cursor = await novelBooks.find(query).toArray();
+      res.send(cursor);
+    });
+  } catch {}
+}
+
+run().catch((error) => {
+  console.log(error);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
